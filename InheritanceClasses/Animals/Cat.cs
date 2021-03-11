0232175b287e4348ec3724c020b34efed8e656c9
daniellejackson <weathers.danielle@ymail.com>
2021-03-11 15:32:04 -0500

    Adding Classes and Inheritance Classes

---
+++ InheritanceClasses/Animals/Cat.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceClasses
+{
+   public class Cat : Animals
+   {
+        
+        public Cat()
+        {
+            Console.WriteLine("This is a cat Constructor.");
+            IsMammal = true;DietType = DietType.Carnivore;
+        }
+        public bool IsEvil { get; set; }
+        public int HowManyWhiskers { get; set; }
+        public string Color { get; set; }
+        public string HairLength { get; set; }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Meow"); 
+        }
+
+        public override void Move ()
+        {
+            Console.WriteLine($"The {GetType().Name} moves quickly!.");
+        }
+   }
+    public class Liger : Cat
+    {
+
+        public Liger()
+        {
+            Console.WriteLine("theis is the Liger Constructor.");
+        }
+        public override void Move()
+        {
+            Console.WriteLine($"The {GetType().Name} stalks its prey.");
+            base.Move();
+        }
+        public override void MakeSound()
+        {
+           Console.WriteLine("Roar");
+        
+        }
+
+
+    }
+
+    public class Lion : Cat
+    {
+        public Lion()
+        {
+            Console.WriteLine("This is a Lion Contsructor");
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Twinkle Toes");
+        }
+    }
+}
---
+++ InheritanceClasses/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InheritanceClasses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("InheritanceClasses")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2832a9d8-b23f-4d0e-905d-58f9887110e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
