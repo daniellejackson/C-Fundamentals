f8cf624f4c4b8d81d4468eb3e7a1575a7866029b
daniellejackson <weathers.danielle@ymail.com>
2021-03-13 23:26:28 -0500

    added conditional

---
+++ MorningChallengeConditionsAndLoops/Classes/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorningChallengeConditionsAndLoops.Classes
+{
+    public class User
+    { 
+    
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+        public User(string firstName, String lastName, int idNumber, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = idNumber;
+            DateOfBirth = dateOfBirth;
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
+
+
+
